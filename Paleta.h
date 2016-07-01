15fdf07e73850e6eb02be3c93a9d40dba8168430
Lenyn Alcántara <nightzpy@gmail.com>
2016-07-01 11:52:31 -0300

    First commid for this old project

---
+++ Paleta.h
@@ -0,0 +1,20 @@
+#ifndef PALETA_H_INCLUDED
+#define PALETA_H_INCLUDED
+
+
+
+#endif // PALETA_H_INCLUDED
+
+class name
+{
+	public:
+
+
+
+	private:
+
+		SDL_Surface *imagen = NULL;
+		double x = 0;
+		double y = 0;
+		double velocidad = 0.0;
+};
